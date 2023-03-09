079142458f3ce68b587e537fd57bf5ab5fd44908
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-03-09 22:01:18 +0200

    refistration

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React from 'react';
+import FormInput from '../../reusableComponents/FormInput/FormInput';
+import { ReactComponent as UserIcon } from './user.svg';
+import { ReactComponent as MailIcon } from './mail.svg';
+import { ReactComponent as LockIcon } from './lock.svg';
+import css from './RegisterForm.module.css';
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const switchImages = name => {
+    switch (name) {
+      case 'name':
+        return <UserIcon />;
+
+      case 'email':
+        return <MailIcon />;
+
+      case 'password':
+        return <LockIcon />;
+
+      default:
+        return <UserIcon className={css.img} />;
+    }
+  };
+  return (
+    <div>
+      <FormInput
+        switchImages={switchImages}
+        placeholder={'name'}
+        type={'name'}
+      />
+      <FormInput
+        switchImages={switchImages}
+        placeholder={'email'}
+        type={'email'}
+      />
+      <FormInput
+        switchImages={switchImages}
+        placeholder={'password'}
+        type={'password'}
+      />
+    </div>
+  );
 };
 
 export default RegisterForm;
---
+++ src/reusableComponents/FormInput/FormInput.jsx
@@ -0,0 +1,20 @@
+import css from './FormInput.module.css';
+import { ReactComponent as SucsessIcon } from './sucsess.svg';
+import { ReactComponent as ErorrIcon } from './erorr.svg';
+const FormInput = ({ placeholder, type, switchImages }) => {
+  return (
+    <div className={css.form__area}>
+      <input
+        className={css.form__input}
+        type={type}
+        name="formImput"
+        placeholder={placeholder}
+      />
+      <span className={css.form__icon}>{switchImages(type)} </span>
+      <SucsessIcon className={css.form__stateIcon} />
+      <ErorrIcon className={css.form__stateIcon} />
+    </div>
+  );
+};
+
+export default FormInput;

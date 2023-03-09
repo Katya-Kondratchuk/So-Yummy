da5fb522c0c3f84df30533977486442d58e8f4ff
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2023-03-09 16:38:23 +0200

    add title

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Title from 'reusableComponents/Title/Title';
 
 const Navigation = () => {
   return (
@@ -11,7 +10,6 @@ const Navigation = () => {
       <NavLink to="/favorite"> FavoriteRecipesPage</NavLink>
       <NavLink to="/shopping-list"> ShoppingListPage</NavLink>
       <NavLink to="/search">SearchPage</NavLink>
-      <Title text={'Main page'} />
     </nav>
   );
 };

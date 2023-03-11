e614ef135a9db59d98ffdb7f427010fb9fecb658
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-03-11 20:04:40 +0200

    update PreviewCategories

--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import css from './PreviewCategories.module.css';
 import Button from 'reusableComponents/Button/Button';
+import DishCard from 'reusableComponents/DishCard/DishCard';
+import { Link } from 'react-router-dom';
 
-const images = [
-  'https://via.placeholder.com/300x300',
-  'https://via.placeholder.com/300x300',
-  'https://via.placeholder.com/300x300',
-  'https://via.placeholder.com/300x300',
-];
+const PreviewCategories = ({ categorie = 'Breakfast' }) => {
+  const [mainMeals, setMeals] = useState([]);
 
-const PreviewCategories = () => {
-  const selectedImages = images.slice(0, 4);
+  axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
+
+  async function getMeals() {
+    try {
+      const response = await axios.get('/filter.php?', {
+        params: {
+          c: `${categorie}`,
+          //c: 'Breakfast'
+        },
+      });
+      return response.data.meals;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    (async function getingMeals() {
+      const res = await getMeals();
+      const meals = res;
+      if (meals) {
+        setMeals(meals);
+      }
+    })();
+    return () => {};
+  }, []);
+
+  const selectedmainMeals = mainMeals.slice(0, 4);
+
   return (
     <div className="container">
       <div className={css.previewContainer}>
-        <h2 className={css.title}>Breakfast</h2>
+        <h2 className={css.title}>{categorie}</h2>
         <ul className={css.list}>
-          {selectedImages.map((imageUrl, index) => (
-            <li key={index} className={css.item}>
-              <img src={imageUrl} alt={`${index + 1}`} className={css.image} />
+          {selectedmainMeals.map(({ strMealThumb, idMeal, strMeal }) => (
+            <li key={idMeal} className={css.item}>
+              <Link to="/recipe">
+                <DishCard
+                  image={strMealThumb}
+                  altText={strMeal}
+                  text={strMeal}
+                />
+              </Link>
+              {/* <img src={imageUrl} alt={`${index + 1}`} className={css.image} /> */}
             </li>
           ))}
         </ul>

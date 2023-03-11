59579474fe0c9d302e5a6a3ebfc1743b7ef05a4b
Borys Supreme <bvs.bromes@gmail.com>
2023-03-11 12:41:53 +0200

    add favorite route

---
+++ src/components/Favorite/Favorite.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecipeCard from 'reusableComponents/RecipeCard/RecipeCard';
+
+const Favorite = () => {
+  return (
+    <div>
+      <RecipeCard />
+    </div>
+  );
+};
+
+export default Favorite;

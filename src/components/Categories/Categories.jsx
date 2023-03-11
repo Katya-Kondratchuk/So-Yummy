152407b2d6b35505b7d0cc7e04c718ed5505953f
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2023-03-11 22:44:26 +0200

    make change

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -7,18 +7,12 @@ import s from './Categories.module.css';
 const Categories = () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  const id = () => {
-    const dateString = Date.now().toString(36);
-    const randomness = Math.random().toString(36).substr(2);
-    return dateString + randomness;
-  };
-
   return (
     <div className="container greensImg">
       <Title text={'Categories'} />
       <ul className={s.categoryList}>
-        {arr.map(item => (
-          <li key={id()} className={s.categoryItem}>
+        {arr.map((item, index) => (
+          <li key={index} className={s.categoryItem}>
             <Link to="/recipe">
               <DishCard
                 image="https://img.theculturetrip.com/wp-content/uploads/2019/12/2aaeed6.jpg"

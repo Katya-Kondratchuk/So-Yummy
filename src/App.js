7c0b631d7bf465c2df897a25beceed3edca4f831
Tetiana Zykova <tatiana.nv96@gmail.com>
2023-03-13 00:02:40 +0200

    add  logic public/privat routs

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import SigninPage from 'pages/SigninPage';
 import PrivateRoute from 'routes/PrivateRoute/PrivateRoute';
 import SharedLayout from 'components/SharedLayout';
 import { ToastContainer } from 'react-toastify';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { refreshUser } from 'redux/auth/authOperation';
 import VerifyPage from 'pages/VerifyPage';
+import LoaderSuspense from 'components/LoaderSuspense/LoaderSuspense';
+import { selectAuthIsRefreshUser } from 'redux/auth/authSelectors';
 import 'react-toastify/dist/ReactToastify.css';
 
 const CategoriesPage = lazy(() => import('./pages/CategoriesPage'));
@@ -20,11 +22,11 @@ const FavoriteRecipesPage = lazy(() => import('pages/FavoriteRecipesPage'));
 const ShoppingListPage = lazy(() => import('pages/ShoppingListPage'));
 const MainPage = lazy(() => import('pages/MainPage'));
 const RecipiesPage = lazy(() => import('pages/RecipiesPage'));
-
 const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 const App = () => {
   const dispatch = useDispatch();
+  const isRefreshUser = useSelector(selectAuthIsRefreshUser);
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -34,67 +36,115 @@ const App = () => {
     <div>
       <ToastContainer position="top-right" autoClose={3000} theme="light" />
 
-      <Routes>
-        <Route
-          path="/verification-token/:verificationToken"
-          element={
-            <PublicRoute component={<VerifyPage />} redirectTo="/main" />
-          }
-        />
-        <Route
-          path="/"
-          element={<PublicRoute component={<WelcomePage />} redirectTo="/" />}
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute component={<RegisterPage />} redirectTo="/main" />
-          }
-        />
-        <Route
-          path="/signin"
-          element={
-            <PublicRoute component={<SigninPage />} redirectTo="/main" />
-          }
-        />
+      {isRefreshUser ? (
+        <LoaderSuspense />
+      ) : (
+        <Routes>
+          <Route
+            path="/verification-token/:verificationToken"
+            element={
+              <PublicRoute component={<VerifyPage />} redirectTo="/main" />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PublicRoute component={<WelcomePage />} redirectTo="/main" />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute component={<RegisterPage />} redirectTo="/main" />
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <PublicRoute component={<SigninPage />} redirectTo="/main" />
+            }
+          />
 
-        <Route path="/" element={<SharedLayout />}>
-          <Route
-            path="/main"
-            index
-            element={<PrivateRoute component={<MainPage />} />}
-          />
-          <Route
-            path="/categories"
-            element={<PrivateRoute component={<CategoriesPage />} />}
-          />
-          <Route
-            path="/search"
-            element={<PrivateRoute component={<SearchPage />} />}
-          />
-          <Route
-            path="/add"
-            element={<PrivateRoute component={<AddRecipesPage />} />}
-          />
-          <Route
-            path="/my"
-            element={<PrivateRoute component={<MyRecipesPage />} />}
-          />
-          <Route
-            path="/favorite"
-            element={<PrivateRoute component={<FavoriteRecipesPage />} />}
-          />
-          <Route
-            path="/shopping-list"
-            element={<PrivateRoute component={<ShoppingListPage />} />}
-          />
-          <Route
-            path="/recipe"
-            element={<PrivateRoute component={<RecipiesPage />} />}
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route
+              path="/main"
+              index
+              element={
+                <PrivateRoute component={<MainPage />} redirectTo="/signin" />
+              }
+            />
+            <Route
+              path="/categories"
+              element={
+                <PrivateRoute
+                  component={<CategoriesPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <PrivateRoute component={<SearchPage />} redirectTo="/signin" />
+              }
+            />
+            <Route
+              path="/add"
+              element={
+                <PrivateRoute
+                  component={<AddRecipesPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+            <Route
+              path="/my"
+              element={
+                <PrivateRoute
+                  component={<MyRecipesPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+            <Route
+              path="/favorite"
+              element={
+                <PrivateRoute
+                  component={<FavoriteRecipesPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+            <Route
+              path="/shopping-list"
+              element={
+                <PrivateRoute
+                  component={<ShoppingListPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+            <Route
+              path="/recipe"
+              element={
+                <PrivateRoute
+                  component={<RecipiesPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <PrivateRoute
+                  component={<NotFoundPage />}
+                  redirectTo="/signin"
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      )}
     </div>
   );
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,22 @@
-import Footer from "components/Footer";
-import Header from "components/Header";
-import React, { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import LoaderSuspense from 'components/LoaderSuspense/LoaderSuspense';
+import React, { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectAuthLoading } from 'redux/auth/authSelectors';
 
 const SharedLayout = () => {
+  const loadind = useSelector(selectAuthLoading);
   return (
     <>
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
+      {!loadind && <Header />}
+
+      <Suspense fallback={<LoaderSuspense />}>
         <Outlet />
       </Suspense>
-      <Footer />
+
+      {!loadind && <Footer />}
     </>
   );
 };

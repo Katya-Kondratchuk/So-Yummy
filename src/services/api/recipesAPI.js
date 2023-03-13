6498873dd775ba92bb7dd6ed60b8b8d2aacac175
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2023-03-13 23:28:18 +0200

    add categories

--- src/services/api/recipesAPI.js
+++ src/services/api/recipesAPI.js
@@ -5,10 +5,33 @@ export const getAllRecipes = async (page = 1, limit = 12, sort = '') => {
     const { data } = await axios.get(
       `/recipes?page=${page}&limit=${limit}&sort=${sort}`,
     );
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (error) {
     console.log(error.message);
+    return null;
+  }
+};
+
+export const getRecipeById = async id => {
+  try {
+    const { data } = await axios.get(`/recipes/${id}`);
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+};
+
+export const getAllCategories = async () => {
+  try {
+    const { data } = await axios.get(`/recipes/category/list`);
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    return null;
   }
 };
 
@@ -21,9 +44,10 @@ export const getCategorieRecipes = async (
     const { data } = await axios.get(
       `/recipes/category/${category}?page=${page}&limit=${limit}`,
     );
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (error) {
     console.log(error.message);
+    return null;
   }
 };

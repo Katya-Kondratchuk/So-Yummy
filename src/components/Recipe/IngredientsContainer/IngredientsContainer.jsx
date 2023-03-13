76816cce0e075b4922d3e9d158724ee6e2227fcd
VoloshynVolodymyr <mailvoloshyn@gmail.com>
2023-03-13 11:22:11 +0200

    shopping list intermediate stage

--- src/components/Recipe/IngredientsContainer/IngredientsContainer.jsx
+++ src/components/Recipe/IngredientsContainer/IngredientsContainer.jsx
@@ -1,17 +1,43 @@
+import ShoppingList from 'components/ShoppingList/ShoppingList';
 import React from 'react';
 import Ingredient from '../Ingredient/Ingredient';
 import RecipeRpeparation from '../RecipeRpeparation/RecipeRpeparation';
 import TitleRecipesList from '../TitleRecipesList/TitleRecipesList';
 
-const IngredientsContainer = ({ image, text, id, description }) => {
+const IngredientsContainer = ({ image, text, id, name, description }) => {
   return (
     <div className="container">
       <TitleRecipesList />
-      <Ingredient image={image} text={text} id={id} description={description} />
-      <Ingredient image={image} text={text} id={id} description={description} />
-      <Ingredient image={image} text={text} id={id} description={description} />
-      <Ingredient image={image} text={text} id={id} description={description} />
+      <Ingredient
+        image={image}
+        text={text}
+        id={id}
+        name={name}
+        description={description}
+      />
+      <Ingredient
+        image={image}
+        text={text}
+        id={id}
+        name={name}
+        description={description}
+      />
+      <Ingredient
+        image={image}
+        text={text}
+        id={id}
+        name={name}
+        description={description}
+      />
+      <Ingredient
+        image={image}
+        text={text}
+        id={id}
+        name={name}
+        description={description}
+      />
       <RecipeRpeparation />
+      <ShoppingList />
     </div>
   );
 };

6fcd53fdbeb3b49cca71dbfd210c26e7a1d90ac2
Katya-Kondratchuk <111919146+Katya-Kondratchuk@users.noreply.github.com>
2023-03-13 09:32:00 +0000

    Deploying to gh-pages from @ Katya-Kondratchuk/So-Yummy@317ef1c03b13e03eb61b653c96205fdb7044c572 🚀

--- static/js/406.ad953d0d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy=self.webpackChunkso_yummy||[]).push([[406],{406:function(e,u,n){n.r(u),n.d(u,{default:function(){return c}});n(2791);var s=n(184),c=function(){return(0,s.jsx)("div",{children:"AddRecipesPage"})}}}]);
-//# sourceMappingURL=406.ad953d0d.chunk.js.map

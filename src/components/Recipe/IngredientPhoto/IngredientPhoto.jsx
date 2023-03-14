f4d27f3fdcc8be9dfddac78d7d84169966b7794d
VoloshynVolodymyr <mailvoloshyn@gmail.com>
2023-03-14 22:32:30 +0200

    Recipe-css was improved

--- src/components/Recipe/IngredientPhoto/IngredientPhoto.jsx
+++ src/components/Recipe/IngredientPhoto/IngredientPhoto.jsx
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import css from './IngredientPhoto.module.css';
-
-// const IngredientPhoto = ({ image }) => {
-//   return (
-//     <div className={css.wrapper}>
-//       <img
-//         src="https://www.themealdb.com/images/media/meals/1549542994.jpg"
-//         alt="ss"
-//       />
-//     </div>
-//   );
-// };
-
-// export default IngredientPhoto;
 import React from 'react';
 import css from './IngredientPhoto.module.css';
 
--- src/components/Recipe/IngredientsContainer/IngredientsContainer.jsx
+++ src/components/Recipe/IngredientsContainer/IngredientsContainer.jsx
@@ -1,48 +1,3 @@
-// import ShoppingList from 'components/ShoppingList/ShoppingList';
-// import React from 'react';
-// import Ingredient from '../Ingredient/Ingredient';
-// import RecipeRpeparation from '../RecipeRpeparation/RecipeRpeparation';
-// import TitleRecipesList from '../TitleRecipesList/TitleRecipesList';
-
-// const IngredientsContainer = ({ image, text, id, name, description }) => {
-//   return (
-//     <div className="container">
-//       <TitleRecipesList />
-//       <Ingredient
-//         image={image}
-//         text={text}
-//         id={id}
-//         name={name}
-//         description={description}
-//       />
-//       <Ingredient
-//         image={image}
-//         text={text}
-//         id={id}
-//         name={name}
-//         description={description}
-//       />
-//       <Ingredient
-//         image={image}
-//         text={text}
-//         id={id}
-//         name={name}
-//         description={description}
-//       />
-//       <Ingredient
-//         image={image}
-//         text={text}
-//         id={id}
-//         name={name}
-//         description={description}
-//       />
-//       <RecipeRpeparation />
-//       <ShoppingList />
-//     </div>
-//   );
-// };
-
-// export default IngredientsContainer;
 import React from 'react';
 import Ingredient from '../Ingredient/Ingredient';
 import RecipeRpeparation from '../RecipeRpeparation/RecipeRpeparation';
@@ -53,7 +8,6 @@ const IngredientsContainer = ({
   instructions = '',
   previewImg = '',
 }) => {
-  // console.log(ingridients, instructions, previewImg);
   return (
     <div className="container">
       <TitleRecipesList />

8541c9997768d0645a61ea8983f0e0c7426d290a
JC4114 <4114114jc@gmail.com>
2023-03-16 00:50:26 +0200

    fun

--- src/components/Header/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/Header/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,9 +1,23 @@
 import css from './ThemeSwitcher.module.css';
 
+const audioLight = new Audio('02036.mp3');
+const audioDark = new Audio('cricket-sound.mp3');
+
 const ThemeSwitcher = () => {
   const handleThemeChange = event => {
     const html = document.querySelector('html');
-    html.classList.toggle('ThemeDark', event.target.checked);
+    const isDark = event.target.checked;
+
+    if (isDark !== html.classList.contains('ThemeDark')) {
+      html.classList.toggle('ThemeDark', isDark);
+
+      const audio = isDark ? audioDark : audioLight;
+      audio.play();
+      setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0;
+      }, 2000);
+    }
   };
 
   return (

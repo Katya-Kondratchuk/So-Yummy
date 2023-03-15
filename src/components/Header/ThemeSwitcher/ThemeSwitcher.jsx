e879509bab56c457e190c48856f4d0000c4742ee
JC4114 <4114114jc@gmail.com>
2023-03-15 21:25:49 +0200

    start

--- src/components/Header/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/Header/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,9 +1,19 @@
 import css from './ThemeSwitcher.module.css';
 
 const ThemeSwitcher = () => {
+  const handleThemeChange = event => {
+    const html = document.querySelector('html');
+    html.classList.toggle('ThemeDark', event.target.checked);
+  };
+
   return (
     <div className={css.container}>
-      <input className={css.input} type="checkbox" id="theme-switcher" />
+      <input
+        className={css.input}
+        type="checkbox"
+        id="theme-switcher"
+        onChange={handleThemeChange}
+      />
       <label className={css.label} htmlFor="theme-switcher">
         <span className={css.span} />
       </label>

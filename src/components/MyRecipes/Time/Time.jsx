5c78637dc7b6f3f233c33a0bd2a0a6d3e3941015
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-03-17 19:52:56 +0200

    del

--- src/components/MyRecipes/Time/Time.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import css from './Time.module.css';
-
-const Time = ({ text }) => {
-  return <p className={css.Time}>{text}</p>;
-};
-
-export default Time;
--- src/components/MyRecipes/TitleRecip/TitleRecip.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import css from './TitleRecip.module.css';
-
-const TitleRecip = ({ text }) => {
-  return <h2 className={css.TitleRecip}>{text}</h2>;
-};
-
-export default TitleRecip;

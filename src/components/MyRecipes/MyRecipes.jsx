20054d956b96945205be19c1092b2895768754eb
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2023-03-19 01:28:05 +0200

    kk

--- src/components/MyRecipes/MyRecipes.jsx
+++ src/components/MyRecipes/MyRecipes.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import BGDots from '../../reusableComponents/BGDots/BGDots';
 import Title from '../../reusableComponents/Title/Title';
-import MyRecipeItem from './MyResipeItem/MyRecipeItem';
-import Pagination from './PaginationCustom/Pagination';
+// import MyRecipeItem from './MyResipeItem/MyRecipeItem';
+// import Pagination from './PaginationCustom/Pagination';
 import css from './MyRecipes.module.css';
 import { getOwnRecipe } from 'services/api/recipesAPI';
 

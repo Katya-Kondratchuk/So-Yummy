6d8f8ebc5b2d812997855875f4c3aae60c5324df
VoloshynVolodymyr <mailvoloshyn@gmail.com>
2023-03-19 12:45:39 +0200

    Recipe

--- src/components/Recipe/IngredientsNumber/IngredientsNumber.jsx
+++ src/components/Recipe/IngredientsNumber/IngredientsNumber.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import css from './IngredientsNumber.module.css';
 
 const IngredientsNumber = ({ measure }) => {
-  return <div className={css.wrapper}>{measure}</div>;
+  return (
+    <>
+      {measure && <div className={css.wrapper}>{measure}</div>}
+      {!measure && <div className={css.wrapper}>To your taste</div>}
+    </>
+  );
 };
 
 export default IngredientsNumber;

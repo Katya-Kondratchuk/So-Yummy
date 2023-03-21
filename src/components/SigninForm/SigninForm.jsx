46a0fd39f8f148b2f9a3d247ef8e4c323ceef730
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-03-21 17:55:37 +0200

    test2

--- src/components/SigninForm/SigninForm.jsx
+++ src/components/SigninForm/SigninForm.jsx
@@ -3,7 +3,7 @@ import css from './SigninForm.module.css';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import switchImages from '../../services/switchImages';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import UserDataForm from 'reusableComponents/UserDataForm/UserDataForm';
 import AuthTitle from 'reusableComponents/authTitle/AuthTitle';
 import AuthImg from 'reusableComponents/AuthImg/AuthImg';
@@ -62,6 +62,7 @@ const SigninForm = () => {
     validationSchema: signinSchema,
     onSubmit: (values, { setSubmitting, resetForm }) => {
       const { email, password } = values;
+      dispatch(loginUser({ email, password }));
       setSubmitting(false);
     },
   });

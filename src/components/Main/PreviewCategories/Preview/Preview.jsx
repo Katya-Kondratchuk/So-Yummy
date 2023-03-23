bc8cad2cafffc0b75ebce65d1bd9d9cdd79dafae
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-03-23 15:23:28 +0200

    fix categories

--- src/components/Main/PreviewCategories/Preview/Preview.jsx
+++ src/components/Main/PreviewCategories/Preview/Preview.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import css from './Preview.module.css';
 import Button from 'reusableComponents/Button/Button';
 import DishCard from 'reusableComponents/DishCard/DishCard';
 import { Link } from 'react-router-dom';
-import { getCategorieRecipes } from 'services/api/recipesAPI';
 
-const Preview = ({ category }) => {
+const Preview = ({ data }) => {
   const [mainMeals, setMainMeals] = useState([]);
   const [isShow, setIsShow] = useState(false);
 
@@ -14,69 +13,58 @@ const Preview = ({ category }) => {
     setIsShow(prevState => !prevState);
   };
 
-  function getRandomFour(mainMeals) {
-    const shuffledArray = mainMeals.sort(() => 0.5 - Math.random());
-    return shuffledArray.slice(0, 4);
-  }
-
-  useEffect(() => {
-    if (!category) {
-      return;
-    }
-    getCategorieRecipes(category).then(({ recipes }) => {
-      setMainMeals(getRandomFour(recipes));
-    });
-  }, [category]);
-
   return (
-    mainMeals.length !== 0 && (
-      <>
-        <div className={`${css.previewContainer} ${'container'}`}>
-          <Link to={`/categories/${category}`} className={css.title}>
-            {category}
-          </Link>
-          <ul className={css.list}>
-            {mainMeals.map(
-              ({
-                category,
-                description,
-                favorite,
-                like,
-                popularity,
-                preview,
-                time,
-                title,
-                _id,
-              }) => (
-                <li key={_id} className={css.item}>
-                  <DishCard
-                    id={_id}
-                    isShow={isShow}
-                    toogle={toogle}
-                    image={preview}
-                    altText={description}
-                    text={title}
-                    favorite={favorite}
-                    like={like}
-                    allData={mainMeals}
-                    setAllData={setMainMeals}
-                    popularity={popularity}
-                  />
-                </li>
-              ),
-            )}
-          </ul>
-        </div>
-        <div className={`${css.buttonContainer} ${'container'}`}>
-          <Button
-            isLink
-            href={`/categories/${category}`}
-            divClassName={css.buttonClass}
-            label={'See all'}
-          />
-        </div>
-      </>
-    )
+    <>
+      <div className={`${css.previewContainer} ${'container'}`}>
+        <Link
+          to={`/categories/${
+            data[0].charAt(0).toUpperCase() + data[0].slice(1)
+          }`}
+          className={css.title}
+        >
+          {data[0].charAt(0).toUpperCase() + data[0].slice(1)}
+        </Link>
+        <ul className={css.list}>
+          {data[1].map(
+            ({
+              category,
+              description,
+              favorite,
+              like,
+              popularity,
+              preview,
+              time,
+              title,
+              _id,
+            }) => (
+              <li key={_id} className={css.item}>
+                <DishCard
+                  id={_id}
+                  isShow={isShow}
+                  toogle={toogle}
+                  image={preview}
+                  altText={description}
+                  text={title}
+                  favorite={favorite}
+                  like={like}
+                  allData={mainMeals}
+                  setAllData={setMainMeals}
+                  popularity={popularity}
+                />
+              </li>
+            ),
+          )}
+        </ul>
+      </div>
+      <div className={`${css.buttonContainer} ${'container'}`}>
+        <Button
+          isLink
+          href={`/categories/${data[0]}`}
+          divClassName={css.buttonClass}
+          label={'See all'}
+        />
+      </div>
+    </>
   );
 };
 

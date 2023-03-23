d91b5715db25ee65165e526d26024ade25f7ab8b
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-03-23 13:16:11 +0200

    fixed

--- src/components/SigninForm/SigninForm.jsx
+++ src/components/SigninForm/SigninForm.jsx
@@ -88,8 +88,10 @@ const SigninForm = () => {
             <div className={css.registrationTitleFormat}>
               <AuthTitle titleText="Sign in" />
             </div>
-            <a href="https://so-yummy.onrender.com/api/users/redirect-google-login">
+            <a
+              href="https://so-yummy.onrender.com/api/users/redirect-google-login"
               className={css.googleAuth}
+            >
               <GoogleIcon />
             </a>
             <UserDataForm

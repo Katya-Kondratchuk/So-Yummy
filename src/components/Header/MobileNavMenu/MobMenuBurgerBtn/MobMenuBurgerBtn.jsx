08dfe8c5730f05a41c9ed04db708ebbf39eb43d3
JC4114 <4114114jc@gmail.com>
2023-03-29 15:04:55 +0300

    burger

--- src/components/Header/MobileNavMenu/MobMenuBurgerBtn/MobMenuBurgerBtn.jsx
+++ src/components/Header/MobileNavMenu/MobMenuBurgerBtn/MobMenuBurgerBtn.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { ReactComponent as MobMenuBtn } from '../../../../assets/images/HeaderMenu/headerMenu.svg';
+import { ReactComponent as MobMenuBtnWhite } from '../../../../assets/images/HeaderMenu/menu-03.svg';
 import css from './MobMenuBurgerBtn.module.css';
 
-const MobMenuBurgerBtn = ({ openMenu }) => {
+const MobMenuBurgerBtn = ({ openMenu, isDarkMode }) => {
   return (
     <button
       className={css.button}
       onClick={() => {
         openMenu();
       }}
+      key={isDarkMode}
     >
-      <MobMenuBtn className={css.svg} />
+      {isDarkMode ? (
+        <MobMenuBtnWhite className={css.svg} />
+      ) : (
+        <MobMenuBtn className={css.svg} />
+      )}
     </button>
   );
 };
